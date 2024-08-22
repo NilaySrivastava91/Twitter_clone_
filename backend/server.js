496c9218a03d587fe302652a99c5f4b9dcfb1350
Nilay Srivastava <nilaysrivastava912089@gmail.com>
2024-08-23 03:40:35 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import path from "path";
+import dotenv from 'dotenv';
+import {v2 as cloudinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+const app=express();
+
+const __dirname=path.resolve()
+
+app.use(express.json({limit:"5mb"}));
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
+app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+
+
+app.listen(8000,()=>{
+    console.log("server is listening on port 8000");
+    connectMongoDB();
+});
